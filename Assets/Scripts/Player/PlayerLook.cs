43b1c1bd1d2ca314b2072c04c4c548d0eb7f877f
Daniel Chen <chen3189@purdue.edu>
2021-05-25 13:54:28 +0800

    #add starter code

---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+    private float xRotation;
+    public float sensitivity = 100f;
+
+    private GameObject player;
+
+    private void Start() {
+        player = GameObject.Find("Player");
+    }
+
+    void Update() {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        player.transform.Rotate(Vector3.up * mouseX);
+
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        xRotation = Mathf.Clamp(xRotation - mouseY, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    private CharacterController controller;
+    public Transform groundCheck;
+    private Vector3 move;
+    private Vector3 velocity;
+    public bool isGrounded;
+
+    public float speed = 6f;
+    public float jumpSpeed = 5f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float gravity = -2f;
+    public float groundDistance = .1f;
+    public LayerMask groundMask;
+
+    public bool flying = true;
+
+    void Start() {
+        controller = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update() {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        move = transform.right * x + transform.forward * z;
+
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            flying = !flying;
+        }
+
+        if (!flying) {
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            if (isGrounded && velocity.y < 0)
+                velocity.y = -2f;
+
+            if (Input.GetButtonDown("Jump") && isGrounded) {
+                velocity.y = jumpSpeed;
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        } else {
+            if (Input.GetButtonDown("Jump")) {
+                move += Vector3.up;
+            } else if (Input.GetButtonDown("Fire3")) {
+                move += Vector3.down;
+            }
+        }
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
